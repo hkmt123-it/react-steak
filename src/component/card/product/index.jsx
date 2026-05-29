05928fb5d88e03928fa0d4b054c3e9d4bc868d7e
hkmt123-it <hikmetseferzade225>
2026-05-29 14:45:11 +0400

    modified

--- src/component/card/product/index.jsx
+++ src/component/card/product/index.jsx
@@ -5,7 +5,7 @@ import Card from "..";
 
 const Products = () => {
   return (
-    <div className="max-w-400 mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7.5">
+    <div className="max-w-400 mx-auto grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-7.5 px-5 py-5">
       {
         steaks.map(item=> <Card 
         key={item.id}
